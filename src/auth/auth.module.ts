fd6a7ce1cbf33470cc9f3305a7c298beed801387
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-08 21:54:23 +0630

    fix/main: fix Import error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UserService } from "src/users/users.service";
+import { PrismaService } from "src/utils/prisma.service";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService,PrismaService],
 })
 export class AuthModule {}
