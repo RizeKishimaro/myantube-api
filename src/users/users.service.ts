1534fa45751fa40e3de7eceb4dd08ccc1b31edcf
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-08 22:49:16 +0630

    feat/main: added email sending features

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,20 +3,29 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { PrismaService } from 'src/utils/prisma.service';
 import { CreateUserDto } from './dto/createuser.dto';
+import { EmailService } from 'src/utils/email.service';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private emailService:EmailService) {}
 
   async createUser(createUserDto: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
-    return this.prisma.user.create({
+    const userInfo = await this.prisma.user.create({
       data: {
         name: createUserDto.name,
         email: createUserDto.email,
         password: hashedPassword,
       },
     });
+    const activationCode = randomUUID();
+    console.log(activationCode);
+    await this.prisma.activationCode.create({
+      data:{code: activationCode, userId: userInfo.id} 
+    })
+    const messageTemplate = `Account Creation Successful please use ${activationCode} to verify your account.`
+    await this.emailService.sendEmail(createUserDto.email,"Activate Your Account",messageTemplate)
   }
 
   async activateAccount(code: string) {
