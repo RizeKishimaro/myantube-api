7f903408494104dfb263bc5eb6fcfa012e793389
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-08 22:52:19 +0630

    feat/main: added email sending features

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./users.service";
 import { UserController } from "./users.controller";
 import { PrismaService } from "src/utils/prisma.service";
+import { EmailService } from "src/utils/email.service";
 
 @Module({
   controllers: [UserController],
-  providers: [UserService,PrismaService],
+  providers: [UserService,PrismaService,EmailService],
 })
 export class UsersModule {}
