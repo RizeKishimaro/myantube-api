bc87eafc165ee464e71cc4c44ae325a67ab4bf1a
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-08 22:54:44 +0630

    feat/main: added email sending features

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UserService } from "src/users/users.service";
 import { PrismaService } from "src/utils/prisma.service";
+import { EmailService } from "src/utils/email.service";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService,PrismaService],
+  providers: [AuthService, UserService,PrismaService,EmailService],
 })
 export class AuthModule {}
