0a7ce0b46cb9b89864b532af7a9e441ab311ea4d
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-08 21:57:14 +0630

    fix/main: fix Import error

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./users.service";
 import { UserController } from "./users.controller";
+import { PrismaService } from "src/utils/prisma.service";
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService,PrismaService],
 })
 export class UsersModule {}
