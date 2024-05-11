b5227410abef710258925db0f9b006dc03b2e82e
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-11 17:52:12 +0630

    feat/develop: testing email limit

--- src/exceptions/throttler.filter.ts
+++ src/exceptions/throttler.filter.ts
@@ -7,19 +7,23 @@ export class ThrottlerCustomExceptionFilter extends BaseExceptionFilter {
   catch(exception: ThrottlerException, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const response = context.getResponse();
+    const request = context.getRequest();
 
     // Calculate retry time as 1 minute (60 seconds) from now
     const now = Date.now(); // Current time in milliseconds
-    const retryAfterMilliseconds = now + 60000; // Current time + 60 seconds (1 minute) in milliseconds
+    let retryAfterMilliseconds = now + 60000;
     const differenceMilliseconds = retryAfterMilliseconds - now; // Difference in milliseconds
+   console.log(request.headers);
+    if(request.headers["retry-after"]){
+      console.log("existed")
+      retryAfterMilliseconds =  request.headers["retry-after"]+ 60000;
+    }
     const retryAfterSeconds = Math.ceil(differenceMilliseconds / 1000); // Convert difference to seconds (rounded up)
 
     // Set Retry-After header in response
-    response.setHeader('Retry-After', retryAfterSeconds.toString());
-
     const statusCode = exception.getStatus();
 
-    response.status(statusCode).json({
+    response.set({"retry-after": retryAfterSeconds.toString()}).status(statusCode).json({
       statusCode,
       message: `Rate limit exceeded. Please try again in ${retryAfterSeconds} seconds.`,
     });
