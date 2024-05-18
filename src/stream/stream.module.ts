f92db553f4e8627ef03326b37782479ebf8892d8
RizeKishimaro <jokerlove2671@gmail.com>
2024-05-18 15:18:31 +0630

    feat/main: video query update

--- src/stream/stream.module.ts
+++ src/stream/stream.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { StreamService } from "./stream.service";
 import { StreamController } from "./stream.controller";
+import { PrismaService } from "../utils/prisma.service";
 
 @Module({
   controllers: [StreamController],
-  providers: [StreamService],
+  providers: [StreamService,PrismaService],
 })
 export class StreamModule {}
