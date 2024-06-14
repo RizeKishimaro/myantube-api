507baa591ca5c8b25f767da810fde89f63745da9
RizeKishimaro <jokerlove2671@gmail.com>
2024-06-14 15:47:00 +0630

    fix/main: connected with webscraper api

--- src/stream/stream.controller.ts
+++ src/stream/stream.controller.ts
@@ -6,9 +6,8 @@ import {
   Req,
   Res,
 } from "@nestjs/common";
+import axios from 'axios';
 import { StreamService } from "./stream.service";
-import { join } from "path";
-import { createReadStream, statSync } from "fs";
 import { PrismaService } from "../utils/prisma.service";
 
 @Controller("stream")
@@ -24,35 +23,55 @@ export class StreamController {
     @Res() response: any,
     @Query("video") id: number,
   ) {
-    const range = request.headers.range ?? "5";
+    const range = request.headers.range ?? '0';
     if (!range) {
       throw new BadRequestException({
         code: 299,
-        message: "Bad range header or does not exist.",
+        message: 'Bad range header or does not exist.',
       });
     }
+
+    // Fetch the video URL from the database
     const query = await this.prisma.video.findFirst({
       where: {
         id: +id,
       },
       select: {
-        url: true,
+        urlHd: true,
+        urlSd: true,
       },
     });
-    const video = query.url;
-    const total_length = statSync(video).size;
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, total_length - 1);
-    const content_length = end - start + 1;
-    const headers = {
-      "Content-Range": `bytes ${start}-${end}/${total_length}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": content_length,
-      "Content-Type": "video/mp4",
-    };
-    const stream = createReadStream(video, { start, end });
-    response.writeHead(206, headers);
-    stream.pipe(response);
+
+    if (!query) {
+      throw new BadRequestException({
+        code: 404,
+        message: 'Video not found.',
+      });
+    }
+
+    const videoUrl = query.urlHd || query.urlSd 
+
+    try {
+      const videoResponse = await axios.get(videoUrl, {
+        responseType: 'stream',
+        headers: {
+          Range: range,
+        },
+      });
+
+      const headers = {
+        'Content-Range': videoResponse.headers['content-range'],
+        'Accept-Ranges': 'bytes',
+        'Content-Length': videoResponse.headers['content-length'],
+        'Content-Type': 'video/mp4',
+      };
+
+      response.writeHead(206, headers);
+
+      videoResponse.data.pipe(response);
+    } catch (error) {
+      console.error('Error streaming video:', error.message);
+      response.status(500).send('Error streaming video');
+    }
   }
 }
