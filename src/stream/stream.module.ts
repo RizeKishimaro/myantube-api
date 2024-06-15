7de8e2260299c36480b8c468916f19a712044eca
RizeKishimaro <jokerlove2671@gmail.com>
2024-06-15 18:16:22 +0630

    fix/main: fx depency error

--- src/stream/stream.module.ts
+++ src/stream/stream.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { StreamService } from "./stream.service";
 import { StreamController } from "./stream.controller";
 import { PrismaService } from "../utils/prisma.service";
+import { FactoryService } from "../factory/factory.service";
 
 @Module({
   controllers: [StreamController],
-  providers: [StreamService, PrismaService],
+  providers: [StreamService, PrismaService,FactoryService],
 })
 export class StreamModule {}
