459c635a129118c3f2f1ca971794f3a718b80ab8
RizeKishimaro <jokerlove2671@gmail.com>
2024-06-21 13:53:49 +0630

    fix/main: fix email send

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -3,9 +3,10 @@ import { VideoService } from "./video.service";
 import { VideoController } from "./video.controller";
 import { PrismaService } from "../utils/prisma.service";
 import { ResponseHelper } from "../utils/responseHelper.service";
+import { FactoryService } from "../factory/factory.service";
 
 @Module({
   controllers: [VideoController],
-  providers: [VideoService, PrismaService, ResponseHelper],
+  providers: [VideoService, PrismaService, ResponseHelper,FactoryService],
 })
 export class VideoModule {}
